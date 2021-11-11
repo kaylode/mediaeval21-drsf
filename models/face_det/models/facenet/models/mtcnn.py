9618f49d40786814c7ceb3f1bbe91010b7e514b3
Kaylode <kayp.kieran@gmail.com>
2021-11-11 23:18:14 +0700

    move pretrained models

--- models/face_det/models/facenet/models/mtcnn.py
+++ models/face_det/models/facenet/models/mtcnn.py
@@ -30,7 +30,7 @@ class PNet(nn.Module):
         self.training = False
 
         if pretrained:
-            state_dict_path = os.path.join(os.path.dirname(__file__), '../pretrained/pnet.pt')
+            state_dict_path = 'demo/pretrained/pnet.pt'
             state_dict = torch.load(state_dict_path)
             self.load_state_dict(state_dict)
 
@@ -75,7 +75,7 @@ class RNet(nn.Module):
         self.training = False
 
         if pretrained:
-            state_dict_path = os.path.join(os.path.dirname(__file__), '../pretrained/rnet.pt')
+            state_dict_path = 'demo/pretrained/rnet.pt'
             state_dict = torch.load(state_dict_path)
             self.load_state_dict(state_dict)
 
@@ -128,7 +128,7 @@ class ONet(nn.Module):
         self.training = False
 
         if pretrained:
-            state_dict_path = os.path.join(os.path.dirname(__file__), '../pretrained/onet.pt')
+            state_dict_path = 'demo/pretrained/onet.pt'
             state_dict = torch.load(state_dict_path)
             self.load_state_dict(state_dict)
 
