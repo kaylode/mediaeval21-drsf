fix gaze vector calculation

tools/eval.py
@@ -70,13 +70,12 @@ class Evaluator:
         for face, gaze_vector in zip(faces, gaze_results):
             self.gaze_model._face3d.postprocess([gaze_vector], face)
 
-        euler_angles1 = faces[0].head_pose_rot.as_euler("XYZ", degrees=True).reshape((1, 3))
-        euler_angles2 = faces[1].head_pose_rot.as_euler("XYZ", degrees=True).reshape((1, 3))
-
-        cosine_dist = paired_cosine_distances(euler_angles1, euler_angles2).mean()
+        euler_angles1 = faces[0].gaze_vector[:2].reshape((1, 2))
+        euler_angles2 = faces[1].gaze_vector[:2].reshape((1, 2))
+        gaze_dist = paired_euclidean_distances(euler_angles1, euler_angles2).mean() 
         angle_error = compute_angle_error(torch.from_numpy(gaze_results[0]).unsqueeze(0), torch.from_numpy(gaze_results[1]).unsqueeze(0))
         angle_error = float(angle_error.item())
-        return cosine_dist, angle_error
+        return gaze_dist, angle_error
 
     def evaluate(self, frame1, frame2):
 
@@ -90,7 +89,7 @@ class Evaluator:
 
         if self.gaze_model is not None:
             gaze_dist, angle_error = self._evaluate_gaze(frame1, frame2, bboxes, landmarks)
-            eval_results['gaze_cosine_dist'] = gaze_dist
+            eval_results['gaze_edist'] = gaze_dist
             eval_results['angle_error'] = angle_error
             eval_results['diffscore'] = (1-iou_score) + lm_dist + angle_error
         return eval_results